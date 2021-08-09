eba6032814e773c74d22a1c339e56d9916f1f1f8
Priyanka Kulkarni <>
2021-08-09 09:16:29 -0700

    Updated the training file + removed unnecessary code

--- train_txt2im.py
+++ train_txt2im.py
@@ -8,8 +8,6 @@ import tensorlayer as tl
 from tensorlayer.prepro import *
 from tensorlayer.cost import *
 import numpy as np
-import scipy
-from scipy.io import loadmat
 import time, os, re, nltk
 
 from utils import *
@@ -19,7 +17,7 @@ import model
 
 tf.disable_eager_execution()
 ###======================== PREPARE DATA ====================================###
-dir =r"D:\My_Stuff\JHU_Stuff\625.742\text-to-image-master\Generative-Text-2-Image\\"
+dir ="..\text-to-image-master\Generative-Text-2-Image\\"
 ###======================== PREPARE DATA ====================================###
 print("Loading data from pickle ...")
 
@@ -41,21 +39,11 @@ with open(dir+ "_caption.pickle", 'rb') as f:
     captions_ids_train, captions_ids_test = pickle.load(f)
 
 
-
-
-
-# images_train_256 = np.array(images_train_256)
-# images_test_256 = np.array(images_test_256)
-
 images_test = np.array(images_test)
 images_train = np.array(images_train)
-# print(n_captions_train, n_captions_test)
-# exit()
 
 ni = int(np.ceil(np.sqrt(batch_size)))
-# os.system("mkdir samples")
-# os.system("mkdir samples/step1_gan-cls")
-# os.system("mkdir checkpoint")
+
 tl.files.exists_or_mkdir("samples/step1_gan-cls")
 tl.files.exists_or_mkdir("samples/step_pretrain_encoder")
 tl.files.exists_or_mkdir("checkpoint")
@@ -167,81 +155,6 @@ def main_train():
                       ["A large red bus."] * n
 
 
-    '''
-    
-    sample_sentence = ["A man walking a dog."] * n + \
-                      ["Two dogs."] * n + \
-                      ["A small black dog."] * n + \
-                      ["A dog playing with frisbee."] * n + \
-                      ["A dog sitting. "] * n + \
-                      ["A woman and dog."] * n + \
-                      ["Brown dog sitting."] * n + \
-                      ["A dog laying on the grass."] * n
-    
-    sample_sentence = ["a public transit bus on an open field."] * n + \
-                      ["A bus."] * n + \
-                      ["A guy walks a dog near some buses."] * n + \
-                      ["The orange bus is travelling down the road."] * n + \
-                      ["A city bus driving into a bus terminal. "] * n + \
-                      ["A bus parked next to a but stop loading area."] * n + \
-                      ["A red double decker bus."] * n + \
-                      ["A large red bus."] * n
-    
-    sample_sentence = ["Some old trains sitting in a train yard."] * n + \
-                      ["A train."] * n + \
-                      ["A woman is walking near trains on tracks."] * n + \
-                      ["a train that is on a long train track."] * n + \
-                      ["A red train is going through a forest. "] * n + \
-                      ["A train traveling down a road in the snow."] * n + \
-                      ["A train at the station."] * n + \
-                      ["A large train."] * n
-    
-    sample_sentence = ["A white airplane in a field."] * n + \
-                      ["A big model of a plane and a train on display."] * n + \
-                      ["A large airplane is flying through the sky."] * n + \
-                      ["An airplane on a runway by a body of water."] * n + \
-                      ["A plane flying high up in the sky. "] * n + \
-                      ["A person that is looking at an airplane."] * n + \
-                      ["A large plane sitting inside of a hangar."] * n + \
-                      ["A small white airplane is parked on the cement."] * n
-    
-    sample_sentence = ["Some old trains sitting in a train yard."] * n + \
-                      ["A train."] * n + \
-                      ["A woman is walking near trains on tracks."] * n + \
-                      ["a train that is on a long train track."] * n + \
-                      ["A red train is going through a dense forest. "] * n + \
-                      ["A train traveling down a road in the snow."] * n + \
-                      ["A train at the station."] * n +\
-                      ["A large train on a track."] * n
-    
-   
-    sample_sentence = ["A man walking a dog."] * n + \
-                      ["Two dogs."] * n + \
-                      ["A small black dog."] * n + \
-                      ["A dog playing with frisbee."] * n + \
-                      ["A dog sitting. "] * n + \
-                      ["A woman and dog."] * n + \
-                      ["Brown dog sitting."] * n +\
-                      ["A dog laying on the grass."] * n
-                      
-    sample_sentence = ["People sitting around a campfire roasting hot dogs."] * n + \
-                      ["An Asian woman eating by a large window."] * n + \
-                      ["A little girl holding a hot dog and a drink."] * n + \
-                      ["A sandwich with a pickle in a container."] * n + \
-                      ["The lunch looks amazing and very colorful"] * n + \
-                      ["Two young women are eating hot dogs while walking down the sidewalk."] * n + \
-                      ["A close up of a person holding an almost empty glass of wine."] * n +\
-                      ["Tray of food disguised in car, on top of a restaurant counter "] * n
-                      
-    sample_sentence = ["the flower shown has yellow anther red pistil and bright red petals."] * n + \
-                      ["this flower has petals that are yellow, white and purple and has dark lines"] * n + \
-                      ["the petals on this flower are white with a yellow center"] * n + \
-                      ["this flower has a lot of small round pink petals."] * n + \
-                      ["this flower is orange in color, and has petals that are ruffled and rounded."] * n + \
-                      ["the flower has yellow petals and the center of it is brown."] * n + \
-                      ["this flower has petals that are blue and white."] * n + \
-                      ["these white flowers have petals that start off white in color and end in a white towards the tips."] * n
-    '''
 
 
     # sample_sentence = captions_ids_test[0:sample_size]
@@ -340,201 +253,7 @@ def main_train():
             tl.files.save_npz(net_g.all_params, name=net_g_name+str(epoch), sess=sess)
             tl.files.save_npz(net_d.all_params, name=net_d_name+str(epoch), sess=sess)
 
-        # if (epoch != 0) and (epoch % 200) == 0:
-        #     sess.run(tf.initialize_variables(adam_vars))
-        #     print("Re-initialize Adam")
 
-#
-# def main_train_encoder():
-#     """ for Style Transfer """
-#     generator_txt2img = model.generator_txt2img_resnet
-#
-#     ## for training
-#     t_real_caption = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name='real_caption_input')
-#     t_z = tf.placeholder(tf.float32, [batch_size, z_dim], name='z_noise')
-#
-#     net_rnn = rnn_embed(t_real_caption, is_train=False, reuse=False)
-#     net_fake_image, _ = generator_txt2img(t_z,
-#                     net_rnn.outputs + tf.random_normal(shape=net_rnn.outputs.get_shape(), mean=0, stddev=0.02), # NOISE ON RNN
-#                     is_train=True, reuse=False, batch_size=batch_size)
-#     net_encoder = z_encoder(net_fake_image.outputs, is_train=True, reuse=False)
-#
-#     ## for evaluation
-#     t_real_image = tf.placeholder('float32', [batch_size, image_size, image_size, 3], name = 'real_image')
-#     net_z = z_encoder(t_real_image, is_train=False, reuse=True)
-#     net_g2, _ = generator_txt2img(net_z.outputs, net_rnn.outputs, is_train=False, reuse=True, batch_size=batch_size)
-#
-#     loss = tf.reduce_mean( tf.square( tf.sub( net_encoder.outputs, t_z) ))
-#     e_vars = tl.layers.get_variables_with_name('z_encoder', True, True)
-#
-#     lr = 0.0002
-#     lr_decay = 0.5      # decay factor for adam, https://github.com/reedscot/icml2016/blob/master/main_cls_int.lua  https://github.com/reedscot/icml2016/blob/master/scripts/train_flowers.sh
-#     decay_every = 100   # https://github.com/reedscot/icml2016/blob/master/main_cls.lua
-#     beta1 = 0.5
-#
-#     with tf.variable_scope('learning_rate'):
-#         lr_v = tf.Variable(lr, trainable=False)
-#
-#     e_optim = tf.train.AdamOptimizer(lr_v, beta1=beta1).minimize(loss, var_list=e_vars )
-#
-#
-#     ###============================ TRAINING ====================================###
-#     sess = tf.InteractiveSession()
-#     tl.layers.initialize_global_variables(sess)
-#
-#     net_g_name = os.path.join(save_dir, 'net_g.npz')
-#     net_encoder_name = os.path.join(save_dir, 'net_encoder.npz')
-#
-#     if load_and_assign_npz(sess=sess, name=net_g_name, model=net_fake_image) is False:
-#         raise Exception("Cannot find net_g.npz")
-#     load_and_assign_npz(sess=sess, name=net_encoder_name, model=net_encoder)
-#
-#     sample_size = batch_size
-#     idexs = get_random_int(min=0, max=n_captions_train-1, number=sample_size, seed=100)
-#     sample_sentence = captions_ids_train[idexs]
-#     sample_sentence = tl.prepro.pad_sequences(sample_sentence, padding='post')
-#     sample_image = images_train[np.floor(np.asarray(idexs).astype('float')/n_captions_per_image).astype('int')]
-#     # print(sample_image.shape, np.min(sample_image), np.max(sample_image), image_size)
-#     # exit()
-#     sample_image = threading_data(sample_image, prepro_img, mode='translation')    # central crop first
-#     save_images(sample_image, [ni, ni], 'samples/step_pretrain_encoder/train__x.png')
-#
-#
-#     n_epoch = 160 * 100
-#     print_freq = 1
-#     n_batch_epoch = int(n_images_train / batch_size)
-#
-#     for epoch in range(0, n_epoch+1):
-#         start_time = time.time()
-#
-#         if epoch !=0 and (epoch % decay_every == 0):
-#             new_lr_decay = lr_decay ** (epoch // decay_every)
-#             sess.run(tf.assign(lr_v, lr * new_lr_decay))
-#             log = " ** new learning rate: %f" % (lr * new_lr_decay)
-#             print(log)
-#             # logging.debug(log)
-#         elif epoch == 0:
-#             log = " ** init lr: %f  decay_every_epoch: %d, lr_decay: %f" % (lr, decay_every, lr_decay)
-#             print(log)
-#
-#         for step in range(n_batch_epoch):
-#             step_time = time.time()
-#             ## get matched text
-#             idexs = get_random_int(min=0, max=n_captions_train-1, number=batch_size)
-#             b_real_caption = captions_ids_train[idexs]
-#             b_real_caption = tl.prepro.pad_sequences(b_real_caption, padding='post')
-#             # ## get real image
-#             # b_real_images = images_train[np.floor(np.asarray(idexs).astype('float')/n_captions_per_image).astype('int')]
-#             # ## get wrong caption
-#             # idexs = get_random_int(min=0, max=n_captions_train-1, number=batch_size)
-#             # b_wrong_caption = captions_ids_train[idexs]
-#             # b_wrong_caption = tl.prepro.pad_sequences(b_wrong_caption, padding='post')
-#             # ## get wrong image
-#             # idexs2 = get_random_int(min=0, max=n_images_train-1, number=batch_size)
-#             # b_wrong_images = images_train[idexs2]
-#             # ## get noise
-#             b_z = np.random.normal(loc=0.0, scale=1.0, size=(sample_size, z_dim)).astype(np.float32)
-#                 # b_z = np.random.uniform(low=-1, high=1, size=[batch_size, z_dim]).astype(np.float32)
-#
-#             ## update E
-#             errE, _ = sess.run([loss, e_optim], feed_dict={
-#                             t_real_caption : b_real_caption,
-#                             t_z : b_z})
-#                             # t_real_image : b_real_images,})
-#
-#             print("Epoch: [%2d/%2d] [%4d/%4d] time: %4.4fs, e_loss: %8f" \
-#                         % (epoch, n_epoch, step, n_batch_epoch, time.time() - step_time, errE))
-#
-#         if (epoch + 1) % 10 == 0:
-#             print(" ** Epoch %d took %fs" % (epoch, time.time()-start_time))
-#             # print(sample_image.shape, t_real_image)
-#             img_gen = sess.run(net_g2.outputs, feed_dict={
-#                                         t_real_caption : sample_sentence,
-#                                         t_real_image : sample_image,})
-#             img_gen = threading_data(img_gen, imresize, size=[64, 64], interp='bilinear')
-#             save_images(img_gen, [ni, ni], 'samples/step_pretrain_encoder/train_{:02d}_g(e(x))).png'.format(epoch))
-#
-#         if (epoch != 0) and (epoch % 5) == 0:
-#             tl.files.save_npz(net_encoder.all_params, name=net_encoder_name, sess=sess)
-#             print("[*] Save checkpoints SUCCESS!")
-#
-#
-# def main_transaltion():
-#     generator_txt2img = model.generator_txt2img_resnet
-#
-#     t_real_caption = tf.placeholder(dtype=tf.int64, shape=[batch_size, None], name='real_caption_input')
-#     t_real_image = tf.placeholder('float32', [batch_size, image_size, image_size, 3], name = 'real_image')
-#
-#     net_rnn = rnn_embed(t_real_caption, is_train=False, reuse=False)
-#     net_z = z_encoder(t_real_image, is_train=False, reuse=False)
-#     net_g, _ = generator_txt2img(net_z.outputs, net_rnn.outputs, is_train=False, reuse=False)
-#
-#     sess = tf.InteractiveSession()
-#     tl.layers.initialize_global_variables(sess)
-#
-#     net_rnn_name = os.path.join(save_dir, 'net_rnn.npz')
-#     net_g_name = os.path.join(save_dir, 'net_g.npz')
-#     net_e_name = os.path.join(save_dir, 'net_encoder.npz')
-#
-#     load_and_assign_npz(sess=sess, name=net_rnn_name, model=net_rnn)
-#     load_and_assign_npz(sess=sess, name=net_g_name, model=net_g)
-#     load_and_assign_npz(sess=sess, name=net_e_name, model=net_z)
-#
-#     ## random images
-#     # idexs = get_random_int(min=0, max=n_captions_train-1, number=batch_size, seed=100)  # train set
-#     # images = images_train[np.floor(np.asarray(idexs).astype('float')/n_captions_per_image).astype('int')]
-#     # sample_sentence = captions_ids_train[idexs]
-#     idexs = get_random_int(min=0, max=n_captions_test-1, number=batch_size, seed=100) # test set
-#     images = images_test[np.floor(np.asarray(idexs).astype('float')/n_captions_per_image).astype('int')]
-#     for i in [0,8,16,24,32,40,48,56]:
-#         images[i] = images_test[1834]     # DONE easy 226
-#         images[i+1] = images_test[620]    # stand on big staff
-#         images[i+2] = images_test[653]    # 653
-#         images[i+3] = images_test[77]   # DONE flying 16 20 2166big 2303ok 2306ok 2311good 2313soso 2317soso  2311(want to change)
-#         images[i+4] = images_test[2167]   # brunch 275 559 2101
-#
-#         images[i+5] = images_test[235]
-#         images[i+6] = images_test[1455]  # 717 402
-#         images[i+7] = images_test[159]  # fat 300  125  159  612
-#         # # train set
-#         # images[i] = images_train[620]
-#         # images[i+1] = images_train[653]
-#         # images[i+2] = images_train[300]
-#         # images[i+3] = images_train[350]
-#         # images[i+4] = images_train[550]
-#         # images[i+5] = images_train[700]
-#         # images[i+6] = images_train[717]
-#         # images[i+7] = images_train[275]
-#     # sample_sentence = captions_ids_test[idexs]
-#     images = threading_data(images, prepro_img, mode='translation')
-#     save_images(images, [ni, ni], 'samples/translation/_reed_method_ori.png')
-#
-#     # all done
-#     sample_sentence = ["This small bird has a blue crown and white belly."] * ni + \
-#                       ["This small yellow bird has grey wings, and a black bill."] * ni + \
-#                       ["This particular bird with a red head and breast and features grey wings."] * ni + \
-#                       ["This black bird has no other colors with a short bill."] * ni + \
-#                       ["An orange bird with green wings and blue head."] * ni + \
-#                       ["A black bird with a red head."] * ni + \
-#                       ["A red body bird with black wings and a gray beak."] * ni + \
-#                       ["A small brown bird with a brown crown has a white belly."] * ni
-#
-#     for i, sentence in enumerate(sample_sentence):
-#         print("seed: %s" % sentence)
-#         sentence = preprocess_caption(sentence)
-#         sample_sentence[i] = [vocab.word_to_id(word) for word in nltk.tokenize.word_tokenize(sentence)] #+ [vocab.end_id]    # add END_ID
-#         # sample_sentence[i] = [vocab.word_to_id(word) for word in sentence]
-#         # print(sample_sentence[i])
-#     sample_sentence = tl.prepro.pad_sequences(sample_sentence, padding='post')
-#
-#     for i in range(1):
-#         img_trans = sess.run(net_g.outputs, feed_dict={
-#                                     t_real_caption : sample_sentence,
-#                                     t_real_image : images,
-#                                     })
-#
-#         save_images(img_trans, [ni, ni], 'samples/translation/_reed_method_tran%d.png' % i)
-#         print("completed %s" % i)
 
 if __name__ == '__main__':
     import argparse
@@ -548,12 +267,6 @@ if __name__ == '__main__':
     if args.mode == "train":
         main_train()
 
-    ## you would not use this part, unless you want to try style transfer on GAN-CLS paper
-    # elif args.mode == "train_encoder":
-    #     main_train_encoder()
-    #
-    # elif args.mode == "translation":
-    #     main_transaltion()
 
 
 
